47655255b5f7528d35c11792408b17437020f130
Mateus Scarin <mateus.scarin@gmail.com>
2022-10-10 15:53:01 -0300

    feat: aula 7 - Compartilhamento de dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  userName = 'Mateus Scarin';
+
+  userData = {
+    email: 'mateus.scarin@gmail.com',
+    role: 'Admin',
+  };
+
   title = 'curso-angular';
 }
---
+++ src/app/components/parent-data/parent-data.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { EmailValidator } from '@angular/forms';
+
+@Component({
+  selector: 'app-parent-data',
+  templateUrl: './parent-data.component.html',
+  styleUrls: ['./parent-data.component.css'],
+})
+export class ParentDataComponent implements OnInit {
+  @Input() name: string = '';
+  @Input() userData!: { email: string; role: string };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
