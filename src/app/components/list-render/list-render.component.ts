f82fc79998affb320879a482e2985b22d79a823d
Mateus Scarin <mateus.scarin@gmail.com>
2022-10-11 14:11:24 -0300

    feat: aula 13 - A importância das interfaces no Angular

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Animal } from 'src/app/Animal';
 
 @Component({
   selector: 'app-list-render',
@@ -6,15 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent implements OnInit {
-  animals = [
-    { name: 'Maly', type: 'Dog' },
-    { name: 'Jhuly', type: 'Dog' },
-    { name: 'Tomas', type: 'Horse' },
-    { name: 'Duke', type: 'Dog' },
-    { name: 'Tom', type: 'Cat' },
+  animals: Animal[] = [
+    { name: 'Maly', type: 'Dog', age: 12 },
+    { name: 'Jhuly', type: 'Dog', age: 5 },
+    { name: 'Tomas', type: 'Horse', age: 7 },
+    { name: 'Duke', type: 'Dog', age: 8 },
+    { name: 'Tom', type: 'Cat', age: 28 },
   ];
+
+  animal: Animal = {
+    name: 'Jorge',
+    type: 'dog',
+    age: 12,
+  };
+
+  animalDetails = '';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  showAge(animal: Animal) {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos`;
+  }
 }
